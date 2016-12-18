d4727361a7b20fb249a151d4040a78d6c4421580
jessedabney <jesse.dabney@gmail.com>
2016-12-17 21:11:13 -0600

    added scripts to get averages of subsamples

---
+++ get_subsampleAverage.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use File::Basename;
+
+my $output;
+GetOptions ( "output=s" => \$output);
+
+my @files = @ARGV; 
+my %data;
+my $refName;
+my $sampnum = 0;
+
+foreach my $file (@files) {
+	open READ, $file or die "couldn't open file: $file, $?!\n";
+	print STDERR "processing $file...\n";
+	while (my $line = <READ>) {
+		my ($refID, $position, $snps, $cov, $value) = split /\t/, $line;
+		if ($value =~ /na/){
+			$value = 0;
+		}
+		$refName = $refID;
+		$data{$position}{"snps"} += $snps;
+		$data{$position}{"cov"} += $cov;
+		$data{$position}{"value"} += $value;
+	}
+	$sampnum++;
+}
+close READ;
+
+open AVERAGE, ">>${output}" or die "couldn't open file for output, $?\n";
+foreach my $position (sort {$a <=> $b} keys %data) {
+	print AVERAGE join ("\t",
+		$refName,
+		$position, 
+		$data{$position}{"snps"} / $sampnum, 
+		$data{$position}{"cov"} / $sampnum);
+	print AVERAGE "\t";
+	printf(AVERAGE "%.9f", $data{$position}{"value"} / $sampnum);
+	print AVERAGE "\n";
+}
+close AVERAGE;
+
---
+++ get_subsampleAverage_genes.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use File::Basename;
+
+my $output;
+GetOptions ( "output=s" => \$output);
+
+my @files = @ARGV; 
+my %data;
+my $sampnum = 0;
+
+foreach my $file (@files) {
+	open READ, $file or die "couldn't open file: $file, $?!\n";
+	print STDERR "processing $file...\n";
+	while (my $line = <READ>) {
+		my ($gene, $snps, $cov, $value) = split /\t/, $line;
+		if ($value =~ /na/){
+			$value = 0;
+		}
+		$data{$gene}{"snps"} += $snps;
+		$data{$gene}{"cov"} += $cov;
+		$data{$gene}{"value"} += $value;
+	}
+	$sampnum++;
+}
+close READ;
+
+open AVERAGE, ">>${output}" or die "couldn't open file for output, $?\n";
+foreach my $gene (sort keys %data) {
+	print AVERAGE join ("\t",
+		$gene, 
+		$data{$gene}{"snps"} / $sampnum, 
+		$data{$gene}{"cov"} / $sampnum);
+	print AVERAGE "\t";
+	printf(AVERAGE "%.9f", $data{$gene}{"value"} / $sampnum);
+	print AVERAGE "\n";
+}
+close AVERAGE;
+
